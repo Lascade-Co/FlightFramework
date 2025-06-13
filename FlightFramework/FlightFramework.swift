b96cf5e336be054da34e56b893f406975fa6009a
Rohit T P <tprohit9@gmail.com>
2025-06-13 19:04:07 +0530

    Initial Commit

---
+++ FlightFramework/FlightFramework.swift
@@ -0,0 +1,9 @@
+//
+//  FlightFramework.swift
+//  FlightFramework
+//
+//  Created by Rohit T P on 13/06/25.
+//
+
+import Foundation
+
---
+++ FlightFrameworkTests/FlightFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightFrameworkTests.swift
+//  FlightFrameworkTests
+//
+//  Created by Rohit T P on 13/06/25.
+//
+
+import Testing
+@testable import FlightFramework
+
+struct FlightFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
